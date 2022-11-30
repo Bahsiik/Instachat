4175776cfb027308518e3b982db8cef61c6c480c
Ayfri <pierre.ayfri@gmail.com>
2022-11-30 15:29:02 +0100

    feat: Add images for other themes.

--- templates/toolbar.php
+++ templates/toolbar.php
@@ -1,18 +1,18 @@
 <?php $title = 'toolbar';
 $css = ['toolbar.css'];
 ?>
-    <div class="toolbar_container">
-        <img src="../static/images/logo-default-theme" alt="logo">
-        <a class="toolbar_item" href="">
-            <span class="material-symbols-outlined">home</span>
-            Accueil
-        </a>
-        <a class="toolbar_item" href="">
-            <span class="material-symbols-outlined">person</span>
-            Profil
-        </a>
-        <a class="toolbar_item" href="">
-            <span class="material-symbols-outlined">group</span>
+	<div class="toolbar_container">
+		<img src="../static/images/logo.png" alt="logo">
+		<a class="toolbar_item" href="">
+			<span class="material-symbols-outlined">home</span>
+			Accueil
+		</a>
+		<a class="toolbar_item" href="">
+			<span class="material-symbols-outlined">person</span>
+			Profil
+		</a>
+		<a class="toolbar_item" href="">
+			<span class="material-symbols-outlined">group</span>
             Amis
         </a>
         <a class="toolbar_item" href="">
