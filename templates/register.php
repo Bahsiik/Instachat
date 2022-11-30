ff9a21984b45c594bff4965b6f73397dc91f3eba
antaww <anta.pizzetta@gmail.com>
2022-11-30 11:13:46 +0100

    feat: controllers folder + register front complete

--- templates/register.php
+++ templates/register.php
@@ -8,17 +8,27 @@ $js = ['register.js'];
     <div class="form-container sign-up-container">
         <form action="#">
             <h1>Créer un compte</h1>
-            <input type="text" placeholder="Pseudo" />
-            <input type="email" placeholder="Email" />
-            <input type="password" placeholder="Mot de passe" />
+            <input name="pseudo" type="text" placeholder="Pseudo" maxlength="20"/>
+            <input name="email" type="email" placeholder="Email" maxlength="320"/>
+            <input name="password" type="password" placeholder="Mot de passe" maxlength="64"/>
+            <input name="confirm-password" type="password" placeholder="Confirmer mot de passe" maxlength="64"/>
+            <fieldset>
+                <legend>Genre</legend>
+                <input type="radio" id="homme" name="genre" value="homme">
+                <label for="homme">Homme</label>
+                <input type="radio" id="femme" name="genre" value="femme">
+                <label for="femme">Femme</label>
+                <input type="radio" id="autre" name="genre" value="autre">
+                <label for="autre">Autre</label>
+            </fieldset>
             <button>S'identifier</button>
         </form>
     </div>
     <div class="form-container sign-in-container">
         <form action="#">
             <h1>Se connecter</h1>
-            <input type="email" placeholder="Email" />
-            <input type="password" placeholder="Mot de passe" />
+            <input type="email" placeholder="Email"/>
+            <input class="login-password" type="password" placeholder="Mot de passe"/>
             <button>Se connecter</button>
         </form>
     </div>
