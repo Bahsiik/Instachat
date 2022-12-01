34c0865644d2ca007a3b542bba033eb5ac1571c6
Ayfri <pierre.ayfri@gmail.com>
2022-12-01 20:11:16 +0100

    feat: Improve theming of inputs.

--- templates/register.php
+++ templates/register.php
@@ -5,73 +5,75 @@ $js = ['register.js'];
 
 <?php ob_start(); ?>
 <div class="container" id="container">
-    <div class="form-container sign-up-container">
-        <form method="post" action="/index.php?create">
-            <h1>Créer un compte</h1>
-            <fieldset>
-                <legend>Email</legend>
-                <input required name="email" type="email" placeholder="Email" minlength="5" maxlength="320"/>
-            </fieldset>
-            <fieldset>
-                <legend>Pseudo</legend>
-                <input required name="username" type="text" placeholder="Pseudo" minlength="2" maxlength="20"/>
-            </fieldset>
-            <fieldset>
-                <legend>Mot de passe</legend>
-                <input required name="password" type="password" placeholder="Mot de passe" minlength="4"
-                       maxlength="64"/>
-            </fieldset>
-            <fieldset>
-                <legend>Confirmez mot de passe</legend>
-                <input required name="confirm-password" type="password" placeholder="Confirmer mot de passe"
-                       minlength="4"
-                       maxlength="64"/>
-            </fieldset>
-            <fieldset>
-                <legend>Genre</legend>
-                <input checked type="radio" id="homme" name="gender" value="homme">
-                <label for="homme">Homme</label>
-                <input type="radio" id="femme" name="gender" value="femme">
-                <label for="femme">Femme</label>
-                <input type="radio" id="autre" name="gender" value="autre">
-                <label for="autre">Autre</label>
-            </fieldset>
-            <fieldset>
-                <legend>Naissance</legend>
-                <input required type="date" id="birthdate" name="birthdate">
-            </fieldset>
-            <button type="submit">S'identifier</button>
-        </form>
-    </div>
-    <div class="form-container sign-in-container">
-        <form method="post" action="/index.php?login">
-            <h1>Se connecter</h1>
-            <fieldset>
-                <legend>Email</legend>
-                <input required type="email" placeholder="Email" name="email" minlength="5" maxlength="320"/>
-            </fieldset>
-            <fieldset>
-                <legend>Mot de passe</legend>
-                <input required class="login-password" type="password" placeholder="Mot de passe" name="password"
-                       minlength="4" maxlength="64"/>
-            </fieldset>
-            <button type="submit">Se connecter</button>
-        </form>
-    </div>
-    <div class="overlay-container">
-        <div class="overlay">
-            <div class="overlay-panel overlay-left">
-                <h1>Re-bonjour !</h1>
-                <p>Pour vous connecter, entrez vos identifiants ici</p>
-                <button class="ghost" id="signIn">Se connecter</button>
-            </div>
-            <div class="overlay-panel overlay-right">
-                <h1>Bienvenue !</h1>
-                <p>Démarrez votre aventure, créez votre compte ici</p>
-                <button class="ghost" id="signUp">S'identifier</button>
-            </div>
-        </div>
-    </div>
+	<div class="form-container sign-up-container">
+		<form action="/index.php?create" method="post">
+			<h1>Créer un compte</h1>
+			<fieldset>
+				<legend>Email</legend>
+				<input maxlength="320" minlength="5" name="email" placeholder="Email" required type="email"/>
+			</fieldset>
+			<fieldset>
+				<legend>Pseudo</legend>
+				<input maxlength="20" minlength="2" name="username" placeholder="Pseudo" required type="text"/>
+			</fieldset>
+			<fieldset>
+				<legend>Mot de passe</legend>
+				<input
+					required name="password" type="password" placeholder="Mot de passe" minlength="4"
+					maxlength="64"
+				/>
+			</fieldset>
+			<fieldset>
+				<legend>Confirmez mot de passe</legend>
+				<input maxlength="64" minlength="4" name="confirm-password" placeholder="Confirmer mot de passe" required type="password"/>
+			</fieldset>
+			<fieldset class="padding">
+				<legend>Genre</legend>
+				<label for="homme">Homme
+					<input checked id="homme" name="gender" type="radio" value="homme">
+				</label>
+				<label for="femme">Femme
+					<input id="femme" name="gender" type="radio" value="femme">
+				</label>
+				<label for="autre">Autre
+					<input id="autre" name="gender" type="radio" value="autre">
+				</label>
+			</fieldset>
+			<fieldset class="padding">
+				<legend>Naissance</legend>
+				<input id="birthdate" name="birthdate" required type="date">
+			</fieldset>
+			<button type="submit">S'identifier</button>
+		</form>
+	</div>
+	<div class="form-container sign-in-container">
+		<form action="/index.php?login" method="post">
+			<h1>Se connecter</h1>
+			<fieldset>
+				<legend>Email</legend>
+				<input maxlength="320" minlength="5" name="email" placeholder="Email" required type="email"/>
+			</fieldset>
+			<fieldset>
+				<legend>Mot de passe</legend>
+				<input class="login-password" maxlength="64" minlength="4" name="password" placeholder="Mot de passe" required type="password"/>
+			</fieldset>
+			<button>Se connecter</button>
+		</form>
+	</div>
+	<div class="overlay-container">
+		<div class="overlay">
+			<div class="overlay-panel overlay-left">
+				<h1>Re-bonjour !</h1>
+				<p>Pour vous connecter, entrez vos identifiants ici</p>
+				<button class="ghost" id="signIn">Se connecter</button>
+			</div>
+			<div class="overlay-panel overlay-right">
+				<h1>Bienvenue !</h1>
+				<p>Démarrez votre aventure, créez votre compte ici</p>
+				<button class="ghost" id="signUp">S'identifier</button>
+			</div>
+		</div>
+	</div>
 </div>
 <?php $content = ob_get_clean(); ?>
 <?php require_once('layout.php'); ?>
