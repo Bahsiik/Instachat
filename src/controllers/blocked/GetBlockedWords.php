9fa674c1b6a8ac3f0828b6ffbaaaec1a5403a269
Bahsiik <o.mist1303@gmail.com>
2022-12-05 16:05:30 +0100

    fix: remove unnecessary arguments

--- src/controllers/blocked/GetBlockedWords.php
+++ src/controllers/blocked/GetBlockedWords.php
@@ -4,14 +4,12 @@ namespace Controllers\Friend;
 
 use Model\BlockedRepository;
 use Model\User;
-use RuntimeException;
 
 class GetBlockedWords
 {
-    public function execute(User $connected_user, array $input): array
+    public function execute(User $connected_user): array
     {
         $friendRepository = new BlockedRepository();
-        if (!isset($input['blocked_id'])) throw new RuntimeException('Invalid input');
-        return $friendRepository->getBlockedWords($connected_user->id, $input['blocked_id']);
+        return $friendRepository->getBlockedWords($connected_user->id);
     }
 }
