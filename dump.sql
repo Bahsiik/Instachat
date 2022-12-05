e6aa2ad5e446bee98e4162647d0b78c30df2d2da
Bahsiik <o.mist1303@gmail.com>
2022-12-05 14:33:25 +0100

    chore: update database dumb

--- dump.sql
+++ dump.sql
@@ -1,4 +1,4 @@
-create table if not exists instachat.users
+create table users
 (
     id           bigint unsigned auto_increment
         primary key,
@@ -17,44 +17,40 @@ create table if not exists instachat.users
 )
     engine = InnoDB;
 
-create table if not exists instachat.blocked
+create table blocked
 (
-    blocker_id bigint unsigned not null
-        primary key,
-    blocked_id bigint unsigned not null,
-    constraint fk_blocked_users
-        foreign key (blocker_id) references instachat.users (id)
-            on update cascade on delete cascade,
-    constraint fk_blocked_users_0
-        foreign key (blocked_id) references instachat.users (id)
-            on update cascade on delete cascade
+    blocker_id   bigint unsigned not null,
+    blocked_id   bigint unsigned null,
+    blocked_word varchar(100)    null,
+    constraint fk_blocked_user
+        foreign key (blocked_id) references users (id),
+    constraint fk_blocker_user
+        foreign key (blocker_id) references users (id)
 )
     engine = InnoDB;
 
 create index idx_blocked_blocked
-    on instachat.blocked (blocked_id);
+    on blocked (blocked_id);
 
-create table if not exists instachat.friends
+create table friends
 (
-    requester_id  bigint unsigned            not null
-        primary key,
     requested_id  bigint unsigned            not null,
     accepted      tinyint(1) default (false) null comment 'null = not viewed, false = rejected, true = accepted',
     send_date     timestamp  default (now()) not null,
     response_date timestamp  default (now()) null,
+    requester_id  bigint unsigned            not null,
     constraint fk_friends_users
-        foreign key (requester_id) references instachat.users (id)
-            on update cascade on delete cascade,
+        foreign key (requester_id) references users (id),
     constraint fk_friends_users_0
-        foreign key (requested_id) references instachat.users (id)
+        foreign key (requested_id) references users (id)
             on update cascade on delete cascade
 )
     engine = InnoDB;
 
 create index idx_friends_requested
-    on instachat.friends (requested_id);
+    on friends (requested_id);
 
-create table if not exists instachat.posts
+create table posts
 (
     id            bigint unsigned auto_increment
         primary key,
@@ -65,12 +61,12 @@ create table if not exists instachat.posts
     emotion       smallint unsigned          not null,
     deleted       tinyint(1) default 0       not null,
     constraint fk_posts_users
-        foreign key (author_id) references instachat.users (id)
+        foreign key (author_id) references users (id)
             on update cascade on delete cascade
 )
     engine = InnoDB;
 
-create table if not exists instachat.comments
+create table comments
 (
     id            bigint unsigned auto_increment
         primary key,
@@ -83,67 +79,66 @@ create table if not exists instachat.comments
     author_id     bigint unsigned              not null,
     deleted       tinyint(1)   default 0       not null,
     constraint fk_comments_comments
-        foreign key (reply_id) references instachat.comments (id)
+        foreign key (reply_id) references comments (id)
             on update cascade on delete cascade,
     constraint fk_comments_posts
-        foreign key (id) references instachat.posts (id)
+        foreign key (id) references posts (id)
             on update cascade on delete cascade,
     constraint fk_comments_users
-        foreign key (author_id) references instachat.users (id)
+        foreign key (author_id) references users (id)
             on update cascade on delete cascade
 )
     engine = InnoDB;
 
 create index idx_comments_post_id
-    on instachat.comments (post_id);
+    on comments (post_id);
 
 create index idx_comments_reply_id
-    on instachat.comments (reply_id);
+    on comments (reply_id);
 
 create index idx_posts_author_id
-    on instachat.posts (author_id);
+    on posts (author_id);
 
-create table if not exists instachat.reactions
+create table reactions
 (
     id      bigint unsigned auto_increment
         primary key,
     post_id bigint unsigned not null,
     emoji   varchar(2)      not null,
     constraint fk_reactions_posts
-        foreign key (post_id) references instachat.posts (id)
+        foreign key (post_id) references posts (id)
             on update cascade on delete cascade
 )
     engine = InnoDB;
 
-create table if not exists instachat.reaction_users
+create table reaction_users
 (
     reaction_id bigint unsigned not null
         primary key,
     user_id     bigint unsigned not null,
     constraint fk_reaction_users_reactions
-        foreign key (reaction_id) references instachat.reactions (id)
+        foreign key (reaction_id) references reactions (id)
             on update cascade on delete cascade,
     constraint fk_reaction_users_users
-        foreign key (user_id) references instachat.users (id)
+        foreign key (user_id) references users (id)
             on update cascade on delete cascade
 )
     engine = InnoDB;
 
 create index idx_reaction_users_user_id
-    on instachat.reaction_users (user_id);
+    on reaction_users (user_id);
 
 create index idx_reactions_post_id
-    on instachat.reactions (post_id);
+    on reactions (post_id);
 
-create table if not exists instachat.sessions
+create table sessions
 (
-    id           varchar(64)               not null
+    id            varchar(64)               not null
         primary key,
-    user_id      bigint unsigned           not null,
-    created_date timestamp default (now()) null,
+    user_id       bigint unsigned           not null,
+    creation_date timestamp default (now()) null,
     constraint fk_user_id
-        foreign key (user_id) references instachat.users (id)
-            on update cascade on delete cascade
+        foreign key (user_id) references users (id)
 )
     engine = InnoDB;
 
