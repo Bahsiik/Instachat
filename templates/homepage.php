b3b34850666e8d3a82c2e04f1be6adff84330577
antaww <anta.pizzetta@gmail.com>
2022-12-06 14:08:48 +0100

    feat: chat area's front done

--- templates/homepage.php
+++ templates/homepage.php
@@ -15,24 +15,35 @@ require_once('toolbar.php');
             <h1>Accueil</h1>
         </div>
         <div class="chat-container">
-            <form class="post-form" action="/chat" method="post">
-                <textarea class="chat-area" placeholder="Chatter quelque chose..." name="content"></textarea>
-                <button type="button"><i class="fa-regular fa-image"></i></button>
-                <div class="emotions">
-                    <?php
-                    for ($i = 1; $i < count(Emotion::cases()) + 1; $i++) {
-                        ?>
-                        <label>
-                            <input type="radio" name="emotion" class="emotion"
-                                   value="<?= $i ?>" <?= $i === 1 ? 'checked' : '' ?> required hidden/>
-                            <span class="emoji-span"><?= Emotion::cases()[$i - 1]->display() ?></span>
-                        </label>
-                        <?php
-                    }
-                    ?>
-                </div>
-                <button type="submit">Chat</button>
-            </form>
+            <div class="chat-avatar">
+                <img src="../static/images/logo.png" alt="avatar">
+            </div>
+            <div class="chat-right">
+                <form class="post-form" action="/chat" method="post">
+                    <textarea class="chat-area" placeholder="Chatter quelque chose..." name="content"
+                              maxlength="400"></textarea>
+                    <div class="chat-form-bottom">
+                        <button class="chat-image-btn">
+                            <span class="material-symbols-outlined chat-action-buttons-color">image</span>
+                        </button>
+                        <div class="emotions">
+                            <?php
+                            for ($i = 1; $i < count(Emotion::cases()) + 1; $i++) {
+                                ?>
+                                <label>
+                                    <input type="radio" name="emotion" class="emotion"
+                                           value="<?= $i ?>" <?= $i === 1 ? 'checked' : '' ?> required hidden/>
+                                    <span class="emoji-span"><?= Emotion::cases()[$i - 1]->display() ?></span>
+                                </label>
+                                <?php
+                            }
+                            ?>
+                        </div>
+                        <button class="chat-btn" type="submit">Chat</button>
+                    </div>
+                </form>
+            </div>
+
         </div>
         <div class="feed-container">
             <?php
