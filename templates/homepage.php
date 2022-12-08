88f9afd465b1a1586bd022b94f7b97390af72068
Ayfri <pierre.ayfri@gmail.com>
2022-12-08 04:29:15 +0100

    feat(theme): Adjust black theme, fix homepage css files.

--- templates/homepage.php
+++ templates/homepage.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 use Controllers\Post\GetComments;
 use Model\Emotion;
 
-array_push($css, 'homepage.css', 'post.css');
+$css = ['homepage.css', 'post.css'];
 $title = 'Instachat';
 $js = ['homepage.js'];
 
