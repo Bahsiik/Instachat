b5e4645fa6dade38fa2cf26244a0ecd204565a25
Ayfri <pierre.ayfri@gmail.com>
2022-12-08 21:25:57 +0100

    fix(trends): Do not call end of page in trends.

--- templates/trends.php
+++ templates/trends.php
@@ -5,51 +5,48 @@ $css[] = 'trends.css';
 $js[] = 'trends.js';
 
 global $trends; ?>
-	<div class="trends-container">
-		<div class="title">
-			<h1>Tendances</h1>
-		</div>
-		<div class="trends-feed">
-			<?php
-			$index = 1;
-			foreach ($trends as $name => $count) {
-				if ($index < 6) {
-					?>
-					<div class="trend">
-						<div class="trend-rank">
-							<p><?= $index . ' • Tendances' ?></p>
-						</div>
-						<div class="trend-content">
-							<p><?= $name ?></p>
-						</div>
-						<div class="trend-number">
-							<p><?= $count . ' chat' . ($count > 1 ? 's' : '') ?></p>
-						</div>
+<div class="trends-container">
+	<div class="title">
+		<h1>Tendances</h1>
+	</div>
+	<div class="trends-feed">
+		<?php
+		$index = 1;
+		foreach ($trends as $name => $count) {
+			if ($index < 6) {
+				?>
+				<div class="trend">
+					<div class="trend-rank">
+						<p><?= "$index • Tendances" ?></p>
 					</div>
-					<?php
-				} else {
-					?>
-					<div class="trend hidden">
-						<div class="trend-rank">
-							<p><?= $index . ' • Tendances' ?></p>
-						</div>
-						<div class="trend-content">
-							<p><?= $name ?></p>
-						</div>
-						<div class="trend-number">
-							<p><?= $count . ' chat' . ($count > 1 ? 's' : '') ?></p>
-						</div>
+					<div class="trend-content">
+						<p><?= $name ?></p>
 					</div>
-					<?php
-				}
-				$index++;
+					<div class="trend-number">
+						<p><?= $count . ' chat' . ($count > 1 ? 's' : '') ?></p>
+					</div>
+				</div>
+				<?php
+			} else {
+				?>
+				<div class="trend hidden">
+					<div class="trend-rank">
+						<p><?= "$index • Tendances" ?></p>
+					</div>
+					<div class="trend-content">
+						<p><?= $name ?></p>
+					</div>
+					<div class="trend-number">
+						<p><?= $count . ' chat' . ($count > 1 ? 's' : '') ?></p>
+					</div>
+				</div>
+				<?php
 			}
-			?>
-			<div class="show-more">
-				<button class="show-more-button" onclick="changeTrendsDisplay()">Afficher plus</button>
-			</div>
+			$index++;
+		}
+		?>
+		<div class="show-more">
+			<button class="show-more-button" onclick="changeTrendsDisplay()">Afficher plus</button>
 		</div>
 	</div>
-<?php
-$content = ob_get_clean();
-require_once('layout.php');
+</div>
