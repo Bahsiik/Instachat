2f43ad42e2ca7f2bfdd5b2af8dced4f8fef2cc71
Ayfri <pierre.ayfri@gmail.com>
2022-12-11 03:46:06 +0100

    fix(post): Fix posting post with image, remove debug message.

--- src/controllers/posts/AddPost.php
+++ src/controllers/posts/AddPost.php
@@ -10,8 +10,8 @@ use function Lib\Utils\redirect;
 
 class AddPost {
 	public function execute(User $connected_user, array $input): void {
-		var_dump(strlen($_FILES['image-content']['tmp_name']) > 0);
-		if (strlen($_FILES['image-content']['tmp_name']) > 0) {
+
+		if ($_FILES['image-content']['tmp_name'] !== '') {
 			$image_src = $_FILES;
 			$image_tmp = $image_src['image-content']['tmp_name'];
 			$image_base64 = base64_encode(file_get_contents($image_tmp));
