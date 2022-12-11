ae5ef078dcdc8e0641700d007164365a6805081b
Ayfri <pierre.ayfri@gmail.com>
2022-12-11 15:09:16 +0100

    feat(post): Limit post image input to image type and 16MB.

--- static/scripts/homepage.js
+++ static/scripts/homepage.js
@@ -1,85 +1,97 @@
+/**
+ * @param textarea {HTMLTextAreaElement}
+ * @param imageInput {HTMLInputElement}
+ * @returns {boolean}
+ */
+function inputsAreValid(textarea, imageInput) {
+    const image = imageInput.files[0];
+    const maxSize = 16 * 1024 * 1024;
+    if (image) {
+        if (image.size > maxSize) {
+            alert('Image size must be less than 16MB');
+            return false;
+        }
+        if (!image.type.startsWith('image/')) {
+            alert('Image must be an image');
+            return false;
+        }
+    }
+
+    return textarea.value.length >= 2 || imageInput.files.length >= 1;
+}
+
 document.addEventListener('DOMContentLoaded', () => {
-	const chatArea = document.querySelector('.chat-area');
-	const characterCount = document.querySelector('.chat-count-number');
-	const characterCountMax = document.querySelector('.chat-count-max');
+    const chatArea = document.querySelector('.chat-area');
+    const characterCount = document.querySelector('.chat-count-number');
+    const characterCountMax = document.querySelector('.chat-count-max');
 
-	class RGB {
-		constructor(r, g, b) {
-			this.r = r;
-			this.g = g;
-			this.b = b;
-		}
-	}
+    class RGB {
+        constructor(r, g, b) {
+            this.r = r;
+            this.g = g;
+            this.b = b;
+        }
+    }
 
-	function updateCharacterCount() {
-		const text = chatArea.value;
-		characterCount.innerHTML = text.length;
-		if (text.length === 0) {
-			characterCount.style.color = 'white';
-			characterCountMax.style.color = 'white';
-		} else {
-			const color = text.length <= 100
-				? new RGB(255, 255, 255 - (text.length / 100) * 255)
-				: new RGB(255, 255 - ((text.length - 100) / 300) * 255, 0);
-			characterCount.style.color = `rgb(${color.r}, ${color.g}, ${color.b})`;
-			characterCountMax.style.color = `rgb(${color.r}, ${color.g}, ${color.b})`;
-		}
-	}
+    function updateCharacterCount() {
+        const text = chatArea.value;
+        characterCount.innerHTML = text.length;
+        if (text.length === 0) {
+            characterCount.style.color = 'white';
+            characterCountMax.style.color = 'white';
+        } else {
+            const color = text.length <= 100
+                ? new RGB(255, 255, 255 - (text.length / 100) * 255)
+                : new RGB(255, 255 - ((text.length - 100) / 300) * 255, 0);
+            characterCount.style.color = `rgb(${color.r}, ${color.g}, ${color.b})`;
+            characterCountMax.style.color = `rgb(${color.r}, ${color.g}, ${color.b})`;
+        }
+    }
 
-	chatArea.addEventListener('input', updateCharacterCount);
+    chatArea.addEventListener('input', updateCharacterCount);
 
-	const form = document.querySelector('.post-form');
-	const textarea = document.querySelector('.chat-area');
-	const imageInput = document.querySelector('.chat-image-input');
-	const chatFormBottom = document.querySelector('.chat-form-bottom');
-	const chatButton = chatFormBottom.querySelector('.chat-btn');
+    const form = document.querySelector('.post-form');
+    const textarea = document.querySelector('.chat-area');
+    const imageInput = document.querySelector('.chat-image-input');
+    const chatFormBottom = document.querySelector('.chat-form-bottom');
+    const chatButton = chatFormBottom.querySelector('.chat-btn');
 
-	textarea.addEventListener('input', () => {
-		updateChatButton();
-	});
-	imageInput.addEventListener('change', () => {
-		updateChatButton();
-	});
+    textarea.addEventListener('input', () => updateChatButton());
+    imageInput.addEventListener('change', () => updateChatButton());
 
-	function updateChatButton() {
-		console.log('updateChatButton');
-		if (textarea.value.length < 2 && imageInput.files.length < 1) {
-			chatButton.setAttribute('disabled', '');
-		} else {
-			chatButton.removeAttribute('disabled');
-		}
-	}
+    function updateChatButton() {
+        if (inputsAreValid(textarea, imageInput)) chatButton.removeAttribute('disabled');
+        else chatButton.setAttribute('disabled', '');
+    }
 
-	// Additional security to check when submitting form
-	form.addEventListener('submit', (e) => {
-		if (textarea.value.length < 2 && imageInput.files.length < 1) {
-			e.preventDefault();
-		}
-	});
+    form.addEventListener('submit', e => {
+        if (inputsAreValid(textarea, imageInput)) return;
+        e.preventDefault();
+    });
 
-	function addImage() {
-		const fileInput = document.querySelector('.chat-image-input');
-		fileInput.click();
-		const reader = new FileReader();
-		fileInput.addEventListener('change', () => {
-			const file = fileInput.files[0];
-			reader.onloadend = () => {
-				const fileType = file.type.split('/')[1];
-				if (fileType !== 'png' && fileType !== 'jpeg' && fileType !== 'jpg') {
-					return;
-				}
-				if (document.querySelector('.chat-image')) {
-					const existingImage = document.querySelector('.chat-image');
-					existingImage.remove();
-				}
-				const img = (`<img class="chat-image" src="${reader.result}" alt="">`);
-				const chatFormImageContainer = document.querySelector('.chat-form-image-container');
-				chatFormImageContainer.innerHTML += img;
-			};
-			reader.readAsDataURL(file);
-		});
-	}
+    function addImage() {
+        const fileInput = document.querySelector('.chat-image-input');
+        fileInput.click();
+        const reader = new FileReader();
+        fileInput.addEventListener('change', () => {
+            const file = fileInput.files[0];
+            reader.onloadend = () => {
+                const fileType = file.type.split('/')[1];
+                if (fileType !== 'png' && fileType !== 'jpeg' && fileType !== 'jpg') return;
 
-	const imageButton = document.querySelector('.chat-image-btn');
-	imageButton.addEventListener('click', addImage);
+                if (document.querySelector('.chat-image')) {
+                    const existingImage = document.querySelector('.chat-image');
+                    existingImage.remove();
+                }
+
+                const img = `<img class="chat-image" src="${reader.result}" alt="">`;
+                const chatFormImageContainer = document.querySelector('.chat-form-image-container');
+                chatFormImageContainer.innerHTML += img;
+            };
+            reader.readAsDataURL(file);
+        });
+    }
+
+    const imageButton = document.querySelector('.chat-image-btn');
+    imageButton.addEventListener('click', addImage);
 });
