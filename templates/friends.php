e8ffabafdd0d19feb533fa5fa6259f2e62b56a73
Bahsiik <o.mist1303@gmail.com>
2022-12-11 17:42:51 +0100

    feat: add text when list are empty on friends page

--- templates/friends.php
+++ templates/friends.php
@@ -13,10 +13,7 @@ $js[] = 'friends.js';
 ob_start();
 require_once 'components/toolbar.php';
 
-global $connected_user;
-global $friend_list;
-global $friend_requests;
-global $sent_requests;
+global $connected_user, $friend_list, $friend_requests, $sent_requests;
 ?>
 	<main class="friends-tab-container">
 		<div class="tabbed-menu">
@@ -25,39 +22,46 @@ global $sent_requests;
 			<div class="tab requests" onclick="showTab(tab3)"><p>Requêtes</p></div>
 		</div>
 		<div class="content">
-
 			<div class="friends-list">
 				<?php
-				foreach ($friend_list as $name => $value) {
-					$friend_id = $connected_user->id == $value->requesterId ? $value->requestedId : $value->requesterId;
-					$friend = $friend_controller->execute($friend_id);
-					?>
-					<div class="friend">
-						<div class="friend-info">
-							<div class="friend-avatar">
-								<a href="/profile/<?= $friend->username ?>">
-									<img src="<?= $friend->displayAvatar() ?>" alt="Avatar">
-								</a>
-							</div>
-							<div class="friend-info-text">
-								<div class="friend-name">
+				if (count($friend_list) > 0) {
+					foreach ($friend_list as $name => $value) {
+						$friend_id = $connected_user->id == $value->requesterId ? $value->requestedId : $value->requesterId;
+						$friend = $friend_controller->execute($friend_id);
+						?>
+						<div class="friend">
+							<div class="friend-info">
+								<div class="friend-avatar">
 									<a href="/profile/<?= $friend->username ?>">
-										<p><?= htmlspecialchars($friend->username) ?></p>
+										<img src="<?= $friend->displayAvatar() ?>" alt="Avatar">
 									</a>
 								</div>
-								<div class="friendship-term">
-									<p>Amis depuis <?php
-										format_date_time_diff($value->responseDate, "le ")?></p>
+								<div class="friend-info-text">
+									<div class="friend-name">
+										<a href="/profile/<?= $friend->username ?>">
+											<p><?= htmlspecialchars($friend->username) ?></p>
+										</a>
+									</div>
+									<div class="friendship-term">
+										<p>Amis depuis <?php
+											format_date_time_diff($value->responseDate, "le ") ?></p>
+									</div>
 								</div>
 							</div>
+							<div class="friend-actions">
+								<form action="/remove-friend" method="post">
+									<input type="hidden" name="friend_id" value="<?= $friend->id ?>">
+									<input type="hidden" name="redirect" value="/friends">
+									<button class="material-symbols-outlined cancel" type="submit">close</button>
+								</form>
+							</div>
 						</div>
-						<div class="friend-actions">
-							<form action="/remove-friend" method="post">
-								<input type="hidden" name="friend_id" value="<?= $friend->id ?>">
-								<input type="hidden" name="redirect" value="/friends">
-								<button class="material-symbols-outlined cancel" type="submit">close</button>
-							</form>
-						</div>
+						<?php
+					}
+				} else {
+					?>
+					<div class="empty">
+						<h2>Liste d'amis vide</h2>
 					</div>
 					<?php
 				}
@@ -66,40 +70,48 @@ global $sent_requests;
 
 			<div class="waiting-list hidden">
 				<?php
-				foreach ($friend_requests as $name => $value) {
-					$friend_id = $connected_user->id == $value->requesterId ? $value->requestedId : $value->requesterId;
-					$friend = $friend_controller->execute($friend_id);
-					?>
-					<div class="friend">
-						<div class="friend-info">
-							<div class="friend-avatar">
-								<a href="/profile/<?= $friend->username ?>">
-									<img src="<?= $friend->displayAvatar() ?>" alt="Avatar">
-								</a>
-							</div>
-							<div class="friend-info-text">
-								<div class="friend-name">
+				if (count($friend_requests) > 0) {
+					foreach ($friend_requests as $name => $value) {
+						$friend_id = $connected_user->id == $value->requesterId ? $value->requestedId : $value->requesterId;
+						$friend = $friend_controller->execute($friend_id);
+						?>
+						<div class="friend">
+							<div class="friend-info">
+								<div class="friend-avatar">
 									<a href="/profile/<?= $friend->username ?>">
-										<p><?= htmlspecialchars($friend->username) ?></p>
+										<img src="<?= $friend->displayAvatar() ?>" alt="Avatar">
 									</a>
 								</div>
-								<div class="friendship-term">
-									<p>Demande reçu</p>
+								<div class="friend-info-text">
+									<div class="friend-name">
+										<a href="/profile/<?= $friend->username ?>">
+											<p><?= htmlspecialchars($friend->username) ?></p>
+										</a>
+									</div>
+									<div class="friendship-term">
+										<p>Demande reçu</p>
+									</div>
 								</div>
 							</div>
+							<div class="friend-actions">
+								<form action="/accept-friend" method="post">
+									<input type="hidden" name="requester_id" value="<?= $friend->id ?>">
+									<input type="hidden" name="redirect" value="/friends">
+									<button class="material-symbols-outlined accept" type="submit">how_to_reg</button>
+								</form>
+								<form action="/decline-friend" method="post">
+									<input type="hidden" name="requester_id" value="<?= $friend->id ?>">
+									<input type="hidden" name="redirect" value="/friends">
+									<button class="material-symbols-outlined cancel" type="submit">close</button>
+								</form>
+							</div>
 						</div>
-						<div class="friend-actions">
-							<form action="/accept-friend" method="post">
-								<input type="hidden" name="requester_id" value="<?= $friend->id ?>">
-								<input type="hidden" name="redirect" value="/friends">
-								<button class="material-symbols-outlined accept" type="submit">how_to_reg</button>
-							</form>
-							<form action="/decline-friend" method="post">
-								<input type="hidden" name="requester_id" value="<?= $friend->id ?>">
-								<input type="hidden" name="redirect" value="/friends">
-								<button class="material-symbols-outlined cancel" type="submit">close</button>
-							</form>
-						</div>
+						<?php
+					}
+				} else {
+					?>
+					<div class="empty">
+						<h2>Aucune demande d'amis</h2>
 					</div>
 					<?php
 				}
@@ -108,35 +120,45 @@ global $sent_requests;
 
 			<div class="requests-list hidden">
 				<?php
-				foreach ($sent_requests as $name => $value) {
-					$friend_id = $connected_user->id == $value->requesterId ? $value->requestedId : $value->requesterId;
-					$friend = $friend_controller->execute($friend_id);
-					?>
-					<div class="friend">
-						<div class="friend-info">
-							<div class="friend-avatar">
-								<a href="/profile/<?= $friend->username ?>">
-									<img src="<?= $friend->displayAvatar() ?>" alt="Avatar">
-								</a>
-							</div>
-							<div class="friend-info-text">
-								<div class="friend-name">
+				if (count($sent_requests) > 0) {
+
+
+					foreach ($sent_requests as $name => $value) {
+						$friend_id = $connected_user->id == $value->requesterId ? $value->requestedId : $value->requesterId;
+						$friend = $friend_controller->execute($friend_id);
+						?>
+						<div class="friend">
+							<div class="friend-info">
+								<div class="friend-avatar">
 									<a href="/profile/<?= $friend->username ?>">
-										<p><?= htmlspecialchars($friend->username) ?></p>
+										<img src="<?= $friend->displayAvatar() ?>" alt="Avatar">
 									</a>
 								</div>
-								<div class="friendship-term">
-									<p>Requête envoyée</p>
+								<div class="friend-info-text">
+									<div class="friend-name">
+										<a href="/profile/<?= $friend->username ?>">
+											<p><?= htmlspecialchars($friend->username) ?></p>
+										</a>
+									</div>
+									<div class="friendship-term">
+										<p>Requête envoyée</p>
+									</div>
 								</div>
 							</div>
+							<div class="friend-actions">
+								<form action="/cancel-friend" method="post">
+									<input type="hidden" name="requested_id" value="<?= $friend->id ?>">
+									<input type="hidden" name="redirect" value="/friends">
+									<button class="material-symbols-outlined cancel" type="submit">close</button>
+								</form>
+							</div>
 						</div>
-						<div class="friend-actions">
-							<form action="/cancel-friend" method="post">
-								<input type="hidden" name="requested_id" value="<?= $friend->id ?>">
-								<input type="hidden" name="redirect" value="/friends">
-								<button class="material-symbols-outlined cancel" type="submit">close</button>
-							</form>
-						</div>
+						<?php
+					}
+				} else {
+					?>
+					<div class="empty">
+						<h2>Vous n'avez envoyé aucune requête d'amis</h2>
 					</div>
 					<?php
 				}
