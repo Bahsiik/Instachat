459bcca4b2653432dd21c30fde6fa76134883935
Bahsiik <o.mist1303@gmail.com>
2022-12-15 18:17:38 +0100

    feat(block): now remove any friends interactions when block someone

--- src/controllers/blocked/BlockUser.php
+++ src/controllers/blocked/BlockUser.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace Controllers\Blocked;
 
 use Models\BlockedRepository;
+use Models\FriendRepository;
 use Models\User;
 use RuntimeException;
 use function Lib\Utils\redirect;
@@ -12,6 +13,7 @@ class BlockUser {
 	public function execute(User $connected_user, array $input): void {
 		if (!isset($input['blocked_id'])) throw new RuntimeException('Invalid input');
 		(new BlockedRepository())->blockUser($connected_user->id, (float)$input['blocked_id']);
+		(new FriendRepository())->removeFriend($connected_user->id, (float)$input['blocked_id']);
 		redirect($input['redirect']);
 	}
 }
