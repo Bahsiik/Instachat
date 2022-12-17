bc810014e130d0f3c8deac54e5ae050cb91ef3a0
Bahsiik <o.mist1303@gmail.com>
2022-12-17 17:22:01 +0100

    feat(block-user): now redirect to user profile if try to access to a post from a blocked user/user that blocked you

--- src/controllers/pages/PostPage.php
+++ src/controllers/pages/PostPage.php
@@ -2,7 +2,9 @@
 
 namespace Controllers\Pages;
 
+use Controllers\Blocked\IsBlocked;
 use Models\PostRepository;
+use Models\UserRepository;
 use Src\Controllers\Reactions\GetReactionsByPost;
 use function Lib\Utils\redirect;
 
@@ -17,13 +19,17 @@ class PostPage {
 	 * @return void - redirects to the post page
 	 */
 	public function execute(array $input): void {
-		global $post_reactions_list;
 		$post_id = $input['id'];
 		if ($post_id === null) redirect('/');
-		$post_reactions_list = (new GetReactionsByPost())->execute((float)$post_id);
 
-		global $post;
+		global $connected_user, $post, $post_reactions_list, $post_author, $is_connected_user_blocked, $is_user_blocked;
 		$post = (new PostRepository())->getPost($post_id);
+		$post_reactions_list = (new GetReactionsByPost())->execute((float)$post_id);
+		$post_author = (new UserRepository())->getUserById($post->authorId);
+		$is_connected_user_blocked = (new IsBlocked())->execute($post->authorId, $connected_user->id);
+		$is_user_blocked = (new IsBlocked())->execute($connected_user->id, $post->authorId);
+
+		if ($is_connected_user_blocked || $is_user_blocked) redirect('/profile/' . $post_author->username);
 
 		require_once 'templates/post.php';
 	}
