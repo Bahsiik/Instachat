1e2445ff286a5eeba937aa16ee515dc0800eac43
Bahsiik <o.mist1303@gmail.com>
2022-12-18 20:05:08 +0100

    doc(posts): add doc to posts controller

--- src/controllers/posts/GetPostsBySearch.php
+++ src/controllers/posts/GetPostsBySearch.php
@@ -6,7 +6,15 @@ namespace Controllers\Posts;
 use Models\PostRepository;
 use function Lib\Utils\filter_blocked_posts;
 
+/**
+ * Class GetPostsBySearch is a controller that gets the posts containing a word
+ */
 class GetPostsBySearch {
+	/**
+	 * execute is the function that gets the posts containing a word
+	 * @param string $input - the word to search
+	 * @return array - the posts containing the word
+	 */
 	public function execute(string $input): array {
 		global $connected_user;
 		$offset = (int)($_GET['offsetSearchedPosts'] ?? 0);
--- src/controllers/posts/GetPostsReactedByUser.php
+++ src/controllers/posts/GetPostsReactedByUser.php
@@ -8,8 +8,16 @@ use Controllers\Blocked\GetUsersThatBlocked;
 use Models\PostRepository;
 use function Lib\Utils\filter_blocked_posts;
 
+/**
+ * Class GetPostsReactedByUser is a controller that gets the posts reacted by a user
+ */
 class GetPostsReactedByUser {
-	public function execute($user_id) {
+	/**
+	 * execute is the function that gets the posts reacted by a user
+	 * @param $user_id - the id of the user
+	 * @return array - the posts reacted by the user
+	 */
+	public function execute($user_id): array {
 		global $connected_user;
 		$offset = (int)($_GET['offsetReactedPosts'] ?? 0);
 		$posts = (new PostRepository())->getPostsReactedByUser($user_id, $offset);
