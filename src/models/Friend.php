d4276528d413103e133fbfbff601a61b9b03786c
Bahsiik <o.mist1303@gmail.com>
2022-12-18 03:47:00 +0100

    doc(friend): add doc for friend model

--- src/models/Friend.php
+++ src/models/Friend.php
@@ -9,11 +9,22 @@ use Database\DatabaseConnection;
 use DateTime;
 use PDO;
 
+/**
+ * Class Friend represents a friend in the database.
+ */
 class Friend {
 	public DateTime $sendDate;
 	public DateTime $responseDate;
 	public bool $accepted;
 
+	/**
+	 * __construct creates a new Friend.
+	 * @param float $requesterId - The id of the requester.
+	 * @param float $requestedId - The id of the requested.
+	 * @param int $accepted - The status of the friend request.
+	 * @param string $sendDate - The date when the friend request was sent.
+	 * @param string $responseDate - The date when the friend request was responded.
+	 */
 	public function __construct(
 		public float $requesterId,
 		public float $requestedId,
@@ -27,18 +38,36 @@ class Friend {
 	}
 }
 
+/**
+ * Class FriendRepository is used to interact with the friends in the database.
+ */
 class FriendRepository {
 	public PDO $databaseConnection;
 
+	/**
+	 * __construct is the constructor of the class
+	 */
 	public function __construct() {
 		$this->databaseConnection = (new DatabaseConnection())->getConnection();
 	}
 
+	/**
+	 * sendRequest sends a friend request to the user with the given id.
+	 * @param float $requester_id - The id of the requester.
+	 * @param float $requested_id - The id of the requested.
+	 * @return void - This function does not return anything.
+	 */
 	public function sendRequest(float $requester_id, float $requested_id): void {
 		$statement = $this->databaseConnection->prepare('INSERT INTO friends (requester_id, requested_id) VALUES (:requester_id, :requested_id)');
 		$statement->execute(compact('requester_id', 'requested_id'));
 	}
 
+	/**
+	 * acceptRequest accepts a friend request.
+	 * @param float $requester_id - The id of the requester.
+	 * @param float $requested_id - The id of the requested.
+	 * @return void - This function does not return anything.
+	 */
 	public function acceptRequest(float $requester_id, float $requested_id): void {
 		$statement = $this->databaseConnection->prepare('UPDATE friends SET accepted = TRUE, response_date = :response_date WHERE requester_id = :requester_id AND requested_id = :requested_id');
 		$statement->execute([
@@ -48,6 +77,12 @@ class FriendRepository {
 		]);
 	}
 
+	/**
+	 * rejectRequest rejects a friend request.
+	 * @param float $requester_id - The id of the requester.
+	 * @param float $requested_id - The id of the requested.
+	 * @return void - This function does not return anything.
+	 */
 	public function rejectRequest(float $requester_id, float $requested_id): void {
 		$statement = $this->databaseConnection->prepare('UPDATE friends SET accepted = FALSE, response_date = :response_date WHERE requester_id = :requester_id AND requested_id = :requested_id');
 		$statement->execute([
@@ -57,16 +92,33 @@ class FriendRepository {
 		]);
 	}
 
+	/**
+	 * removeFriend removes a friend from the database.
+	 * @param float $requester_id - The id of the requester.
+	 * @param float $requested_id - The id of the requested.
+	 * @return void - This function does not return anything.
+	 */
 	public function removeFriend(float $requester_id, float $requested_id): void {
 		$statement = $this->databaseConnection->prepare('DELETE FROM friends WHERE (requester_id = :requester_id AND requested_id = :requested_id) OR (requester_id = :requested_id AND requested_id = :requester_id)');
 		$statement->execute(compact('requester_id', 'requested_id'));
 	}
 
+	/**
+	 * cancelRequest cancels a friend request.
+	 * @param float $requester_id - The id of the requester.
+	 * @param float $requested_id - The id of the requested.
+	 * @return void - This function does not return anything.
+	 */
 	public function cancelRequest(float $requester_id, float $requested_id): void {
 		$statement = $this->databaseConnection->prepare('DELETE FROM friends WHERE requester_id = :requester_id AND requested_id = :requested_id');
 		$statement->execute(compact('requester_id', 'requested_id'));
 	}
 
+	/**
+	 * getFriends gets all friends of the user with the given id.
+	 * @param float $user_id - The id of the user.
+	 * @return array - An array of friends.
+	 */
 	public function getFriends(float $user_id): array {
 		$statement = $this->databaseConnection->prepare('SELECT * FROM friends WHERE (requester_id = :user_id OR requested_id = :user_id) AND accepted = TRUE');
 		$statement->execute(compact('user_id'));
@@ -85,6 +137,11 @@ class FriendRepository {
 		return $friends;
 	}
 
+	/**
+	 * getFriendRequests gets all friend requests of the user with the given id.
+	 * @param float $user_id - The id of the user.
+	 * @return array - An array of friend requests.
+	 */
 	public function getFriendRequests(float $user_id): array {
 		$statement = $this->databaseConnection->prepare('SELECT * FROM friends WHERE requested_id = :user_id AND accepted IS NULL');
 		$statement->execute(compact('user_id'));
@@ -103,6 +160,11 @@ class FriendRepository {
 		return $friends;
 	}
 
+	/**
+	 * getSentRequests gets all sent friend requests of the user with the given id.
+	 * @param float $user_id - The id of the user.
+	 * @return array - An array of sent friend requests.
+	 */
 	public function getSentRequests(float $user_id): array {
 		$statement = $this->databaseConnection->prepare('SELECT * FROM friends WHERE requester_id = :user_id AND accepted IS NULL');
 		$statement->execute(compact('user_id'));
