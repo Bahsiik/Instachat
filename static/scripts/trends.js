5a4b1b21c85cd4113dbfb17e3ae5156f25dc81e6
Ayfri <pierre.ayfri@gmail.com>
2022-12-18 16:48:46 +0100

    fix(trends): Remove error when there are no "more" button.

--- static/scripts/trends.js
+++ static/scripts/trends.js
@@ -1,24 +1,26 @@
-const button = document.getElementsByClassName("show-more-button")[0];
-button.addEventListener("click", changeTrendsDisplay);
+document.addEventListener('DOMContentLoaded', () => {
+	const button = document.getElementsByClassName("show-more-button")[0];
+	button?.addEventListener("click", changeTrendsDisplay);
 
-function changeTrendsDisplay() {
-	const text = event.target.className;
-	switch (text) {
-		case "show-more-button":
-			const hiddenTrends = document.getElementsByClassName("trend hidden");
-			[...hiddenTrends].forEach(trend => trend.classList.remove("hidden"));
-			document.getElementsByClassName("show-more-button")[0].innerText = "Afficher moins";
-			document.getElementsByClassName("show-more-button")[0].className = "show-less-button";
-			break;
-		case "show-less-button":
-			const trendsToHide = document.getElementsByClassName("trend");
-			[...trendsToHide].forEach((trend, index) => {
-				if (index > 4) {
-					trend.classList.add("hidden");
-				}
-			}, 0);
-			document.getElementsByClassName("show-less-button")[0].innerText = "Afficher plus";
-			document.getElementsByClassName("show-less-button")[0].className = "show-more-button";
-			break;
+	function changeTrendsDisplay(event) {
+		const text = event.target.className;
+		switch (text) {
+			case "show-more-button":
+				const hiddenTrends = document.getElementsByClassName("trend hidden");
+				[...hiddenTrends].forEach(trend => trend.classList.remove("hidden"));
+				document.getElementsByClassName("show-more-button")[0].innerText = "Afficher moins";
+				document.getElementsByClassName("show-more-button")[0].className = "show-less-button";
+				break;
+			case "show-less-button":
+				const trendsToHide = document.getElementsByClassName("trend");
+				[...trendsToHide].forEach((trend, index) => {
+					if (index > 4) {
+						trend.classList.add("hidden");
+					}
+				}, 0);
+				document.getElementsByClassName("show-less-button")[0].innerText = "Afficher plus";
+				document.getElementsByClassName("show-less-button")[0].className = "show-more-button";
+				break;
+		}
 	}
-}
+});
