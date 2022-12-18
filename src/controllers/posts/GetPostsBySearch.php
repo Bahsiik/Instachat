7fc4f195b620377497c834a977e7e0dff4be6ab1
Bahsiik <o.mist1303@gmail.com>
2022-12-18 19:25:32 +0100

    feat(search): remove the post containing blocked words & by blocked users

--- src/controllers/posts/GetPostsBySearch.php
+++ src/controllers/posts/GetPostsBySearch.php
@@ -3,10 +3,31 @@ declare(strict_types=1);
 
 namespace Controllers\Posts;
 
+use Controllers\Blocked\GetBlockedUsers;
+use Controllers\Blocked\GetBlockedWords;
 use Models\PostRepository;
 
 class GetPostsBySearch {
 	public function execute(string $input): array {
-		return (new PostRepository())->getPostsBySearch($input, (int)($_GET['offsetSearchedPosts'] ?? 0));
+		global $connected_user, $blocked_users, $blocked_words;
+		$offset = (int)($_GET['offsetSearchedPosts'] ?? 0);
+		$posts = (new PostRepository())->getPostsBySearch($input, $offset);
+		$blocked_words = (new GetBlockedWords())->execute($connected_user);
+		foreach ($posts as $post) {
+			foreach ($blocked_words as $blocked_word) {
+				if ($post->authorId !== $connected_user->id && mb_stripos(mb_strtolower($post->content), mb_strtolower($blocked_word->blockedWord)) !== false) {
+					unset($posts[array_search($post, $posts, true)]);
+				}
+			}
+		}
+		$blocked_users = (new GetBlockedUsers())->execute($connected_user);
+		foreach ($posts as $post) {
+			foreach ($blocked_users as $blocked_user) {
+				if ($post->authorId === $blocked_user->blockedId) {
+					unset($posts[array_search($post, $posts, true)]);
+				}
+			}
+		}
+		return $posts;
 	}
 }
