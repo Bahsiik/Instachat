3346a72c81790742ccd02478700f6e43bb71e8a1
Bahsiik <o.mist1303@gmail.com>
2022-12-18 01:43:55 +0100

    fix(profile): fix the display name

--- templates/profile.php
+++ templates/profile.php
@@ -5,18 +5,15 @@ $css = ['profile.css', 'comment.css', 'reaction.css', 'navbar.css'];
 $js = ['fetch-feed.js', 'profile.js', 'posts.js', 'comments.js', 'tabbed-menu.js'];
 
 global $connected_user, $user, $friend_list, $friend_requests, $sent_requests, $friendship, $user_posts, $user_comments, $user_reactions, $posts_reacted, $is_connected_user_blocked, $is_user_blocked, $trends;
-$username = htmlspecialchars($user->username);
 
-$title = "Instachat | $username";
+$title = htmlspecialchars($user->username);
 
 ob_start();
 require_once 'components/navbar.php';
-
-
 ?>
 	<main class="profile-container">
 		<div class="title">
-			<h1>Profil de <?= $username ?></h1>
+			<h1>Profil de <?= htmlspecialchars($user->username) ?></h1>
 		</div>
 		<div class="profile-info-container">
 			<div class="profile-info">
@@ -26,18 +23,18 @@ require_once 'components/navbar.php';
 				<div class="profile-info-middle">
 					<div class="profile-info-username">
 						<p class="display-name"><?= $user->getDisplayOrUsername() ?></p>
-						<p class="username">@<?= $username ?></p>
+						<p class="username">@<?= htmlspecialchars($user->username) ?></p>
 					</div>
 					<?php
 					if ($is_user_blocked) {
 					?>
 				</div>
-				<h2>Vous avez bloqué @<?= $username ?>.</h2>
+				<h2>Vous avez bloqué @<?= htmlspecialchars($user->username) ?>.</h2>
 				<?php
 				} else if ($is_connected_user_blocked) {
 				?>
 			</div>
-			<h2>@<?= $username ?> vous a bloqué.</h2>
+			<h2>@<?= htmlspecialchars($user->username) ?> vous a bloqué.</h2>
 			<?php
 			} else {
 			?>
@@ -86,14 +83,14 @@ require_once 'components/navbar.php';
 				?>
 				<form action="/remove-friend" method="post">
 					<input type="hidden" name="friend_id" value="<?= $user->id ?>">
-					<input type="hidden" name="redirect" value="/profile/<?= $username ?>">
+					<input type="hidden" name="redirect" value="/profile/<?= htmlspecialchars($user->username) ?>">
 					<button class="material-symbols-outlined cancel" type="submit" title="Supprimer cet ami">
 						person_remove
 					</button>
 				</form>
 				<form action="/block-user" method="post">
 					<input type="hidden" name="blocked_id" value="<?= $user->id ?>">
-					<input type="hidden" name="redirect" value="/profile/<?= $username ?>">
+					<input type="hidden" name="redirect" value="/profile/<?= htmlspecialchars($user->username) ?>">
 					<button class="material-symbols-outlined block" type="submit" title="Bloquer cet utilisateur">
 						block
 					</button>
@@ -103,21 +100,21 @@ require_once 'components/navbar.php';
 				?>
 				<form action="/accept-friend" method="post">
 					<input type="hidden" name="requester_id" value="<?= $user->id ?>">
-					<input type="hidden" name="redirect" value="/profile/<?= $username ?>">
+					<input type="hidden" name="redirect" value="/profile/<?= htmlspecialchars($user->username) ?>">
 					<button class="material-symbols-outlined cancel" type="submit" title="Accepter la demande d'ami">
 						how_to_reg
 					</button>
 				</form>
 				<form action="/decline-friend" method="post">
 					<input type="hidden" name="requester_id" value="<?= $user->id ?>">
-					<input type="hidden" name="redirect" value="/profile/<?= $username ?>">
+					<input type="hidden" name="redirect" value="/profile/<?= htmlspecialchars($user->username) ?>">
 					<button class="material-symbols-outlined cancel" type="submit" title="Refuser la demande d'ami">
 						close
 					</button>
 				</form>
 				<form action="/block-user" method="post">
 					<input type="hidden" name="blocked_id" value="<?= $user->id ?>">
-					<input type="hidden" name="redirect" value="/profile/<?= $username ?>">
+					<input type="hidden" name="redirect" value="/profile/<?= htmlspecialchars($user->username) ?>">
 					<button class="material-symbols-outlined block" type="submit" title="Bloquer cet utilisateur">
 						block
 					</button>
@@ -127,14 +124,14 @@ require_once 'components/navbar.php';
 				?>
 				<form action="/cancel-friend" method="post">
 					<input type="hidden" name="requested_id" value="<?= $user->id ?>">
-					<input type="hidden" name="redirect" value="/profile/<?= $username ?>">
+					<input type="hidden" name="redirect" value="/profile/<?= htmlspecialchars($user->username) ?>">
 					<button class="material-symbols-outlined cancel" title="Annuler votre demande d'ami" type="submit">
 						close
 					</button>
 				</form>
 				<form action="/block-user" method="post">
 					<input type="hidden" name="blocked_id" value="<?= $user->id ?>">
-					<input type="hidden" name="redirect" value="/profile/<?= $username ?>">
+					<input type="hidden" name="redirect" value="/profile/<?= htmlspecialchars($user->username) ?>">
 					<button class="material-symbols-outlined block" type="submit" title="Bloquer cet utilisateur">
 						block
 					</button>
@@ -144,14 +141,14 @@ require_once 'components/navbar.php';
 				?>
 				<form action="/send-friend-request" method="post">
 					<input type="hidden" name="requested_id" value="<?= $user->id ?>">
-					<input type="hidden" name="redirect" value="/profile/<?= $username ?>">
+					<input type="hidden" name="redirect" value="/profile/<?= htmlspecialchars($user->username) ?>">
 					<button class="material-symbols-outlined cancel" title="Envoyer une demande d'ami" type="submit">
 						person_add
 					</button>
 				</form>
 				<form action="/block-user" method="post">
 					<input type="hidden" name="blocked_id" value="<?= $user->id ?>">
-					<input type="hidden" name="redirect" value="/profile/<?= $username ?>">
+					<input type="hidden" name="redirect" value="/profile/<?= htmlspecialchars($user->username) ?>">
 					<button class="material-symbols-outlined block" type="submit" title="Bloquer cet utilisateur">
 						block
 					</button>
@@ -168,12 +165,12 @@ require_once 'components/navbar.php';
 		<div class="profile-bottom-container">
 			<?php
 			if ($is_user_blocked) { ?>
-				<h2>Vous avez bloqué @<?= $username ?>. Par conséquent, vous ne voyez aucun contenu relatif à ce compte. Pour revoir le contenu de cet
+				<h2>Vous avez bloqué @<?= htmlspecialchars($user->username) ?>. Par conséquent, vous ne voyez aucun contenu relatif à ce compte. Pour revoir le contenu de cet
 					utilisateur, débloquez le depuis les options.</h2>
 				<?php
 			} else if ($is_connected_user_blocked) {
 				?>
-				<h2><?= $username ?> vous a bloqué. Par conséquent, vous ne pouvez accéder à aucun contenu de ce compte.</h2>
+				<h2><?= htmlspecialchars($user->username) ?> vous a bloqué. Par conséquent, vous ne pouvez accéder à aucun contenu de ce compte.</h2>
 				<?php
 			} else {
 				?>
