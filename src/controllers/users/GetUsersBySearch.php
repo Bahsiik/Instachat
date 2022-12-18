1850f49faa596c34a0267c8f646f0924a3c27195
Bahsiik <o.mist1303@gmail.com>
2022-12-18 20:06:11 +0100

    doc(users): add doc to users controller

--- src/controllers/users/GetUsersBySearch.php
+++ src/controllers/users/GetUsersBySearch.php
@@ -5,8 +5,16 @@ namespace Controllers\Users;
 
 use Models\UserRepository;
 
+/**
+ * Class GetUsersBySearch is a controller that gets the users by a search
+ */
 class GetUsersBySearch {
 
+	/**
+	 * execute is the function that gets the users by a search
+	 * @param string $input - the input of the request
+	 * @return array - the users
+	 */
 	public function execute(string $input): array {
 		return (new UserRepository())->getUsersBySearch($input);
 	}
