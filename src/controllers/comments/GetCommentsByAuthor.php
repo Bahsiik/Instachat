a16e8aad6aff87b9a189e6d778bbd44a14db4aea
Bahsiik <o.mist1303@gmail.com>
2022-12-18 20:01:48 +0100

    doc(comments): add doc to comments controller

--- src/controllers/comments/GetCommentsByAuthor.php
+++ src/controllers/comments/GetCommentsByAuthor.php
@@ -6,8 +6,16 @@ use Controllers\Blocked\GetBlockedWords;
 use Models\CommentRepository;
 use function Lib\Utils\filter_blocked_posts;
 
+/**
+ * Class GetCommentsByAuthor is a controller that gets all the comments of an author
+ */
 class GetCommentsByAuthor {
 
+	/**
+	 * execute is the function that gets all the comments of an author
+	 * @param float $author_id - the id of the author
+	 * @return array - the comments of the author
+	 */
 	public function execute(float $author_id): array {
 		global $connected_user;
 		$offset = (int)($_GET['offsetComments'] ?? 0);
